02848a995322c03455877d74765e8aa6bedac12b
Kritin P <79003711+Kritinp@users.noreply.github.com>
2022-11-24 23:24:11 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+import math
+# Error Code to signify a value is uncomputable
+
+
+class trig_calculator:
+    def __init__(self):
+        print("Starting")
+
+    def tan(self, x):
+        return math.tan(x)
+
+    def sin(self, x):
+        return math.sin(x)
+
+    def cos(self, x):
+        return math.cos(x)
